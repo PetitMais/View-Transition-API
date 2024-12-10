e6c5d1b010629a2b483d8c22fb19379a940c8b41
PetitMais <hana.belin@gmail.com>
2024-12-10 15:53:42 +0100

    fix : view-transition.js innerHTML

--- assets/js/components/view-transition.js
+++ assets/js/components/view-transition.js
@@ -1,14 +1,17 @@
 class ViewTransition extends HTMLElement {
+    static observedAttributes = ['content','url'];
 
     connectedCallback() {
         // Affichage du HTML dans index.html.
+        this.content = this.getAttribute('content');
+        this.url = this.getAttribute('url');
+
         this.innerHTML = /* HTML */ `
         <h1>View Transition API</h1>
         <baseline-status featureId="view-transitions"></baseline-status>
-        <p>Description de l'api</p>
-        <a href="exemple.html">Exemple</a>
-        `
-    }
+        <p>${this.content}</p>
+        <a href="${this.url}" id="transition">Exemple</a>
+        `}
 
     disconnectedCallback() {
         console.log('Déconnecter');
