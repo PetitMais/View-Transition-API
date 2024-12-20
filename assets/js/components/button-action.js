74b33cfada24a3c3e9d9d0a9d62eed78766cb5cc
PetitMais <hana.belin@gmail.com>
2024-12-20 21:58:24 +0100

    fix : l'url générique and add : css

--- assets/js/components/button-action.js
+++ assets/js/components/button-action.js
@@ -1,17 +1,44 @@
 class ButtonAction extends HTMLElement {
-    static observedAttributes = ['content'];
+    static observedAttributes = ['content', 'url'];
 
     connectedCallback() {
         // Affichage du HTML dans index.html.
         this.content = this.getAttribute('content');
+        this.url = this.getAttribute('url');
 
         this.innerHTML = /* HTML */ `
-        <a href="description.html">${this.content}</a>
+        <style>
+        .action-local{
+        padding: 10px 25px;
+        background-color: var(--blue);
+        border-radius: 15px;
+        box-shadow: 5px 5px var(--indigo);
+        text-decoration: none;
+        color: black;
+        display: inline-block;
+        width: 100%;
+        }
+        .action-local:hover{
+        padding: 10px 25px;
+        background-color: var(--blue);
+        border-radius: 15px;
+        box-shadow: none;
+        transition:box-shadow .2s;
+        }
+        </style>
+        <a href="${this.url}" class="action-local">${this.content}</a>
         `
+        this.querySelector("a").addEventListener("click", async (event) => {
+            event.preventDefault(); // Empêche le rechargement de la page
+            // Redirection vers la page description
+            window.location.href = `${this.url}?css=${this.content}`;
+        });
     }
+
+
 
     disconnectedCallback() {
         console.log('Déconnecter');
     }
 }
-customElements.define("button-action", ButtonAction);
+customElements.define("button-action", ButtonAction)
